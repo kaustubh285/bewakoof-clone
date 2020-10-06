0d31181652db0940e052878b185a43410c76d93e
kaustubh <kaustubh285@gmail.com>
2020-10-06 13:47:28 +0530

    Home first ad section complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import LocalMallOutlinedIcon from "@material-ui/icons/LocalMallOutlined";
+import PhoneAndroidOutlinedIcon from "@material-ui/icons/PhoneAndroidOutlined";
 
 function Header() {
   return (
@@ -10,7 +11,10 @@ function Header() {
         <div className='header__small1'>
           <p>Offers</p>
           <p>Fanbook</p>
-          <p>Download App</p>
+          <p>
+            <PhoneAndroidOutlinedIcon />
+            Download App
+          </p>
           <p>TriBe Membership</p>
         </div>
         <div className='header__small2'>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import firstAdData from "../ads";
+import AdCard from "../components/AdCard";
+import "./home.css";
 
 function Home() {
+  console.log(firstAdData);
   return (
-    <div>
+    <div className='container'>
+      <div className='AdCard_container'>
+        {firstAdData.map((Ad, key) => (
+          <AdCard Ad={Ad} key={key} />
+        ))}
+      </div>
       {/* Ad Posters */}
       {/* | 4 posters Grid */}
       {/* | Single Poster */}
